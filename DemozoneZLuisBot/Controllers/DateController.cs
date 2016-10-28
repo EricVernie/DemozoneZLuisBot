406d5c710601a2dfe61450a736200772b428bcc5
ericv <ericv@EVW550SB3>
2016-10-28 17:44:37 +0200

    Adding controller

---
+++ DemozoneZLuisBot/Controllers/DateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DemozoneZLuisBot.Controllers
+{
+    public class DateController : ApiController
+    {
+        [HttpGet]
+        public async Task<string> Get([FromBody]string Jour)
+        {
+            
+            return "Bonjour";
+        }
+    }
+}
